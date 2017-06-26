649103333368b574f9243c19abc6c0212fec8ef4
amattn <hidden@example.com>
2017-06-26 11:38:48 -0700

    basic command line flags

--- cmd/embin/main.go
+++ cmd/embin/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 	"runtime"
 	"time"
 )
+
+var show_h bool
+var show_help bool
+var show_version bool
+
+func init() {
+	flag.BoolVar(&show_h, "h", false, "show help message and exit(0)")
+	flag.BoolVar(&show_help, "help", false, "show help message and exit(0)")
+	flag.BoolVar(&show_version, "version", false, "show version info and exit(0)")
+}
 
 func main() {
 	start := time.Now()
@@ -15,5 +27,17 @@ func main() {
 
 	log.Printf("Starting embin (%v, v%v, build %v)", runtime.Version(), Version(), BuildNumber())
 
-	log.Println("Hello World")
+	// command line flags:
+	flag.Parse()
+
+	if show_version {
+		os.Exit(0)
+	}
+
+	if show_h || show_help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	// okay let's actually do stuff
 }
